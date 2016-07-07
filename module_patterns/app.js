e84fdc21965702790b3e6a6cfe14c086486b6417
Raghu <raghureddy.ram@gmail.com>
2016-07-06 21:59:38 -0700

    notes on the discrepencies

--- module_patterns/app.js
+++ module_patterns/app.js
@@ -8,11 +8,11 @@ var greet3 = require('./greet3');
 greet3.greet();
 greet3.greeting = 'Changed hello world!';
 
-var greet3b = require('./greet3');
+var greet3b = require('./greet3'); //require('.greet3') is cached by node so the object is passed by reference
 greet3b.greet();
 
 var Greet4 = require('./greet4');
-var grtr = new Greet4();
+var grtr = new Greet4(); // constructor is invoked outside of the module
 grtr.greet();
 
 var greet5 = require('./greet5').greet;
